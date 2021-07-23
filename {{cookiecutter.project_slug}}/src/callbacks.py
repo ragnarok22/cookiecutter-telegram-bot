556cc3069703e3064c4a7e47c6d8051a8eccb786
Reinier Hernández <sasuke.reinier@gmail.com>
2021-07-23 14:32:18 -0400

    arreglando error en el callback de greetings

--- {{cookiecutter.project_slug}}/src/callbacks.py
+++ {{cookiecutter.project_slug}}/src/callbacks.py
@@ -5,7 +5,7 @@ def greetings_callback(update, context):
     query = update.callback_query
     query.answer()
 
-    query.edit_message_text(
+    query.reply_text(
         text='¿Cómo te llamas?'
     )
     return constants.GREETINGS
